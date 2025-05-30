32740e34dd314de4ea041180c40419f5d91d1b7e
BekLg <bereketlegesse22@gmail.com>
2025-05-30 16:42:29 +0300

    Update frontend config and App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+const backendWsUrl = process.env.REACT_APP_BACKEND_WS_URL;
+
+if (!backendUrl || !backendWsUrl) {
+  console.error('REACT_APP_BACKEND_URL or REACT_APP_BACKEND_WS_URL is not defined in .env file');
+  throw new Error('REACT_APP_BACKEND_URL or REACT_APP_BACKEND_WS_URL is not defined in .env file');
+}
+
+
+
 function App() {
   const [clientId] = useState(uuidv4());
   const [messages, setMessages] = useState([]);
   const [result, setResult] = useState(null);
 
   useEffect(() => {
-    const ws = new WebSocket(`ws://localhost:8000/ws/${clientId}`);
+    const ws = new WebSocket(`${backendWsUrl}/ws/${clientId}`);
     ws.onmessage = (event) => {
       const message = event.data;
 
@@ -32,7 +42,7 @@ function App() {
 
   const startProcess = async () => {
     setMessages([]);
-    await fetch(`http://localhost:8000/process?client_id=${clientId}&gse_id=${gseId}`, {
+    await fetch(`${backendUrl}/process?client_id=${clientId}&gse_id=${gseId}`, {
       method: 'POST',
     });
   };
@@ -88,7 +98,7 @@ function App() {
           </div>
           <div className="card" style={{ flex: 1 }}>
             <div className="card-header">GSE Metadata Predicates</div>
-            <p>{result?.gse_metadata_predicate || "\n\n \t GSE metadata predicates will appear here once ready. \n\n"}</p>
+            <p>{result?.gse_metadata_predicates || "\n\n \t GSE metadata predicates will appear here once ready. \n\n"}</p>
           </div>
         </div>
       </div>
